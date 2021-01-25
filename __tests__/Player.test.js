a3f8ff23a05cd157503dd7c659538a5403ea2d01
CoCo Chanel <veta583518@gmail.com>
2021-01-24 21:04:17 -0600

    write getInventory and getStat functions

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -12,6 +12,25 @@ test('creates a player object', () => {
     );
 });
 
+test("gets player's stats as an object", () => {
+    const player = new Player('Dave');
+
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+});
+
+test('get inventory from player or returns false', () => {
+    const player = new Player('Dave');
+
+    expect(player.getInventory()).toEqual(expect.any(Array));
+
+    player.inventory = [];
+
+    expect(player.getInventory()).toEqual(false);
+});
+
 const Potion = require('../lib/Potion');
 
 jest.mock('../lib/Potion');
